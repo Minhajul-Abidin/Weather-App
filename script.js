4b87e49b1e192bfb9654cd84034b1ef0649f9dae
Minhajul-Abidin <113845913+Minhajul-Abidin@users.noreply.github.com>
2024-02-20 01:44:43 +0530

    Dynamic update

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 const apiKey = "52626d047638616cab0e1279159472a8";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q={city name}";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=";
 
-async function checkWeather() {
-    const responce = await fetch(apiUrl + `&appid=${apiKey}`);
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+
+async function checkWeather(city) {
+  const responce = await fetch(apiUrl + city + `&appid=${apiKey}`);
+  var data = await responce.json();
+
+  console.log(data);
+
+  var tempCelsius = (data.main.temp - 273.15).toFixed(2);
+
+  document.querySelector(".city").innerHTML = data.name;
+  document.querySelector(".temp").innerHTML = tempCelsius + "°C";
+  document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+  document.querySelector(".wind").innerHTML = data.wind.speed + " km/hr";
 }
+
+searchBtn.addEventListener("click", ()=>{
+    checkWeather(searchBox.value);
+});
