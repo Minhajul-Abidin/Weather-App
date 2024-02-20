d2ff1b7b0de9b44fbf2e79a568d9603815e5322f
Minhajul-Abidin <113845913+Minhajul-Abidin@users.noreply.github.com>
2024-02-20 12:32:59 +0530

    Almost there

--- script.js
+++ script.js
@@ -3,19 +3,30 @@ const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=";
 
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon")
 
 async function checkWeather(city) {
   const responce = await fetch(apiUrl + city + `&appid=${apiKey}`);
   var data = await responce.json();
 
-  console.log(data);
-
-  var tempCelsius = (data.main.temp - 273.15).toFixed(2);
-
   document.querySelector(".city").innerHTML = data.name;
-  document.querySelector(".temp").innerHTML = tempCelsius + "°C";
+  document.querySelector(".temp").innerHTML = Math.round((data.main.temp - 273.15)) + "°C";
   document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
   document.querySelector(".wind").innerHTML = data.wind.speed + " km/hr";
+
+  if (data.weather[0].main == "Clouds") {
+    weatherIcon.src = "./images/clouds.png";
+  }else if (data.weather[0].main == "Clear") {
+    weatherIcon.src = "./images/clear.png";
+  }else if (data.weather[0].main == "Rain") {
+    weatherIcon.src = "./images/rain.png";
+  }else if (data.weather[0].main == "Snow") {
+    weatherIcon.src = "./images/snow.png";
+  }else if (data.weather[0].main == "Drizzle") {
+    weatherIcon.src = "./images/drizzle.png";
+  }else if (data.weather[0].main == "Mist") {
+    weatherIcon.src = "./images/mist.png";
+  }
 }
 
 onload = checkWeather("Mumbai");
